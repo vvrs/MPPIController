b4b8fee28169c6b511886a30947bc4c17d413ebf
vvrs <vishnu.rudrasamudram@gmail.com>
2018-04-12 22:13:36 -0400

    changed cost function

--- MPPIControl.m
+++ MPPIControl.m
@@ -3,7 +3,9 @@ function [t_, states_history, u_opt] = MPPIControl(DynamicModel, param, running_
 	states_history = [];
 	% to generate uncertainty in 'u'
 	v = zeros(param.N-1, size(param.u,2));
-
+	u = param.u;
+	x_ = param.x_t0;
+	t_ = param.t0;
 	% optimal input
 	u_opt = zeros(1,size(param.u,2));
 
@@ -19,20 +21,20 @@ function [t_, states_history, u_opt] = MPPIControl(DynamicModel, param, running_
 			x_0 = param.x_t0(end,:);
 			t_0 = param.t0(end,:);
 
-			x_ = x_0;
-			t_ = t_0;
+			% x_ = x_0;
+			% t_ = t_0;
 			% cost of individual trajectories
 			for n = 2:1:param.N-1
 
 				% adding noise to 'u'
 				if k < (1 - param.alpha)*param.K   
-	                v(n-1,:) = param.u(n-1,:) + noise(:,n-1,k)';            
+	                v(n-1,:) = u(n-1,:) + noise(:,n-1,k)';            
 	            else
 	                v(n-1,:) = noise(:,n-1,k)';     
 	            end
 
 	            % bound on 'u'
-	            for i=1:1:size(param.u,2)
+	            for i=1:1:size(u,2)
 	                if  v(n-1,i) > param.u_UB(i)
 	                    v(n-1,i) = param.u_UB(i); 
 	                elseif v(n-1,i) < param.u_LB(i)
@@ -41,18 +43,24 @@ function [t_, states_history, u_opt] = MPPIControl(DynamicModel, param, running_
 	            end
 
 				dW = normrnd(0,param.sigma*ones(1,size(param.x_t0,2)),[1,size(param.x_t0,2)]);
-				x_ = x_ + DynamicModel(t_0, x_0 ,v(n-1,:), dW)*param.dT;
-				t_ = t_ + param.dT;
+				% x_c = x_0 + DynamicModel(t_0, x_0 ,v(n-1,:), dW)*param.dT;
+				% t_c = t_0 + param.dT;
+
+				[t_c, x_c] = ode45(@(t,x)DynamicModel(t, x ,v(n-1,:),dW), [t_0 t_0+param.dT], x_0);
+
+				x_0 = x_c(end,:);
+				t_0 = t_c(end,:);
 
 				% Calculate the cost
-	            [cost error] = running_cost(x_,param.x_desired);
+	            [cost error] = running_cost(x_,param.x_desired,u_opt(1));
 
-	            S(k) = S(k) + cost + param.gamma*param.u(n-1,:)*inv(diag(ones(1, size(param.u,2)))*param.sigma)*v(n-1,:)';
+	            S(k) = S(k) + cost + param.gamma*u(n-1,:)*inv(diag(ones(1, size(u,2)))*param.sigma)*v(n-1,:)';
 
 			end
 	        S(k) = S(k) + terminal_cost(x_0,param.x_desired);
 
-			states_history = [states_history,x_];
+			states_history = [states_history,x_'];
+			% size(x_')
 	        disp(['Sample: ', num2str(k), ' iteration: ', num2str(loop)...
 	            , ' x: ' num2str(x_(1)), ' m',...
 	            ' x_dot: ' num2str(x_(2)),...
@@ -78,34 +86,43 @@ function [t_, states_history, u_opt] = MPPIControl(DynamicModel, param, running_
 	    end
 
 	    for n=1:1:param.N
-	        param.u(n,:) = param.u(n,:) + weights_vector(n,:);
+	        u(n,:) = u(n,:) + weights_vector(n,:);
 
 	        % applying bounds on 'u'
-	        for i = 1:1:size(param.u,2)
-	            if(param.u(n,i) < param.u_LB(i))
-	                param.u(n,i) = param.u_LB(i);
-	            elseif (param.u(n,i) > param.u_UB(i))
-	                param.u(n,i) = param.u_UB(i);
+	        for i = 1:1:size(u,2)
+	            if(u(n,i) < param.u_LB(i))
+	                u(n,i) = param.u_LB(i);
+	            elseif (u(n,i) > param.u_UB(i))
+	                u(n,i) = param.u_UB(i);
 	            end
 	        end
 	    end
 
 	    if mod(loop,1) == 0
-	    	u_opt = [u_opt; param.u(1,:)];
+	    	u_opt = [u_opt; u(1,:)];
+
 
 	    	dW = normrnd(0,param.sigma*ones(1,size(param.x_t0,2)),[1,size(param.x_t0,2)]);
-			x_next = x_0 + DynamicModel(t_0, x_0 ,param.u(1,:), dW)*param.dT;
-			t_next = t_0 + param.dT;
+			% x_next = x_0 + DynamicModel(t_0, x_0 ,param.u(1,:), dW)*param.dT;
+			% t_next = t_0 + param.dT;
 
-			param.x_t0 = x_next;
-			param.t_0  = t_next;
+			% next_state = x_ + DynamicModel(t_, x_ ,param.u(1,:), dW)*param.dT;
+			% t_f = t_ + param.dT;
+
+			[t_f, next_state] = ode45(@(t,x)DynamicModel(t, x ,u(1,:),dW), [t_ t_+param.dT], x_);
+
+			x_ = next_state(end,:);
+			t_ = t_f(end,:);
+
+			% param.x_t0 = x_next;
+			% param.t_0  = t_next;
 
 	    	for n = 1:1:param.N-1
-	            param.u(n,:) = param.u(n+1,:);
+	            u(n,:) = u(n+1,:);
 	        end
 	        
 	        %     Initialize the new last input with a zero
-	        param.u(param.N,:) = 0;
+	        u(param.N,:) = 0;
 	    end
 	end
 end
--- MPPIInvPendulum.m
+++ MPPIInvPendulum.m
@@ -4,7 +4,7 @@ close all;
 
 % Parameters
 param.N = 20; % length of finite horizon
-param.K = 50; % # samples 
+param.K = 100; % # samples 
 param.iterations = 100; % replacement to while(task_completed)
 param.x_t0 = [0 0 pi 0]; % initial state
 param.x_desired = [0 0 0 0]; % final state
@@ -26,27 +26,33 @@ param.mu = 0;
 % control parameters
 param.lambda = 1; % temperature parameter (?)
 param.alpha = 0.05; % Base distribution (0-1) (?)
-param.gamma = param.lambda*(1-param.alpha) % inv(H)*G (?)
+param.gamma = param.lambda*(1-param.alpha); % inv(H)*G (?)
 
-[t_all, x_all, u_all] = MPPIControl(@DynamicModel, param, @running_cost, @terminal_cost)
+[t_all, x_all, u_all] = MPPIControl(@DynamicModel, param, @running_cost, @terminal_cost);
 
 figure;
 plot(u_all);
 
 figure;
+legend;
 plot(x_all(1,:));
-title('State: x');
+hold on
+plot(x_all(2,:));
+hold on
+plot(x_all(3,:));
+hold on
+plot(x_all(4,:));
 
 
 
-function [J, error] = running_cost(x, x_desired)
+function [J, error] = running_cost(x, x_desired,u)
 
-% calculate the cost
  J_(1) = 0*(x(1))^2 ;
  J_(2) = 500*(1 + cos(x(3)+pi))^2;
 %  J_(2) = 500*(x(3))^2;
  J_(3) = 0*(x(2))^2;
  J_(4) = 15*(x(4))^2;
+ J_(5) = u*u;
  J = sum(J_);
  
  error(1) = sqrt((x(1) - x_desired(1))^2);
@@ -57,11 +63,7 @@ end
 
 function [J, error] = terminal_cost(x, x_desired)
 
-J_(1) = 0*(x(1))^2;
-J_(2) = 5000*(1 + cos(x(3)+pi))^2;
-J_(3) = 0*(x(2))^2;
-J_(4) = 100*(x(4))^2;
-J = sum(J_);
+
 J = 0;
  error(1) = 0*sqrt((x(1) - x_desired(1))^2);
  error(2) = 0*sqrt((x(2) - x_desired(2))^2);
