676cb96ba3ac1a8383e3a3adcec783747cf1bb72
rajnishti <rajnish.tiwarivns@gmail.com>
2018-04-23 02:20:20 -0400

    FInal Code
    Good Result

--- cost_function.m
+++ cost_function.m
@@ -12,6 +12,7 @@ function [S] = cost_function(p, p_dot, theta, theta_dot, u)
 %      S = (10*abs(p)^2 + 100*(1+cos(theta))^2 + 0.1*theta_dot^2 + 0.1*p_dot^2)*dt/10;
 %     result S = (10*abs(p)^2 + 100*(1+cos(theta))^2 + 1*theta_dot^2 + 1*p_dot^2)*dt/10;
     S = (20*abs(p)^2 + 100*(1+cos(theta))^2 + 1*theta_dot^2 + 1*p_dot^2)*dt/10;
+%     S = (1*abs(p)^2 + 500*(1+cos(theta))^2 + 1*theta_dot^2 + 1*p_dot^2)*dt/10;
      if p>8
          S = S + 0;
      end
